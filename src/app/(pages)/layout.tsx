3094abccdebebcbafb1ce71410ba9d9918586b9a
seramg <leelasera19@gmail.com>
2026-02-15 20:21:25 +0530

    chore: Moved providers to a separate file

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../../globals.css";
-  import { ToastContainer, toast } from 'react-toastify';
+import Providers from "@/providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ToastContainer />
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
---
+++ src/app/providers.tsx
@@ -0,0 +1,14 @@
+// app/providers.tsx
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+import { ToastContainer } from "react-toastify";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <SessionProvider>
+      <ToastContainer />
+      {children}
+    </SessionProvider>
+  );
+}
